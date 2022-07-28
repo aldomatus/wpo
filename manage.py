e5cfcc41efef4e42e5cf8ef6f75c9aa9ded7c8c6
aldomatus1 <aldo.matus@envioclick.com>
2022-07-28 07:36:51 -0500

    change config dir

--- manage.py
+++ manage.py
@@ -9,7 +9,7 @@ environ.Env.read_env()
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wpo_backend.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
