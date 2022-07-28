a40d24c1a84bf268b2039ecc6e14361b0467c0fa
aldomatus1 <aldo.matus@envioclick.com>
2022-07-27 20:51:06 -0500

    host changed for mysql

--- wpo_backend/settings.py
+++ wpo_backend/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': os.environ["DATABASE_NAME"],
         'USER': os.environ["DATABASE_USER"],
         'PASSWORD': os.environ["DATABASE_PASSWORD"],
-        'HOST': '127.0.0.1',
+        'HOST': os.environ["DATABASE_HOST"],
         'PORT': os.environ["DATABASE_PORT"]
     }
 }
