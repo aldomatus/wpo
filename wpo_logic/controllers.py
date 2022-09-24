4d22482084479c09d7348de81fa815be5389e806
aldomatus <aldo.matus@outlook.com>
2022-09-23 19:03:01 -0500

    Select specific zip code

--- wpo_logic/controllers.py
+++ wpo_logic/controllers.py
@@ -2,4 +2,4 @@ from wpo_logic.models import CodigosPostales
 
 
 def get_all_codigos_postales(key_word):
-    return CodigosPostales.objects.filter(codigo_postal__icontains=key_word)
+    return CodigosPostales.objects.filter(codigo_postal__exact=key_word)
